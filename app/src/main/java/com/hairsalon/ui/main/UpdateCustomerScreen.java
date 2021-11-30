df5e4df914b314b8e5108985ebb084b373766566
Ryder <rdeback@gmail.com>
2021-11-30 14:18:39 -0800

    Added the update customer screen.

---
+++ app/src/main/java/com/hairsalon/ui/main/UpdateCustomerScreen.java
@@ -0,0 +1,18 @@
+package com.hairsalon.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hairsalon.R;
+
+public class UpdateCustomerScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update_customer_screen);
+    }
+}
